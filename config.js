88b96571a62f34c21023da9b87ea5c7103f181cb
yoondo123 <ekaqo0220@gmail.com>
2026-06-04 18:04:13 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // MOONDO123 Supabase config
 // GitHub Pages에 index.html, admin.html과 함께 올리세요.
 const SUPABASE_URL = 'https://ewqicunyawgcwdrcutqf.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3cWljdW55YXdnY3dkcmN1dHFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1NTIwMzIsImV4cCI6MjA5NjEyODAzMn0.OP8lSa7lA8JCYmg1WWJKcMM8EOGBd-G6z8pS-TqXE2U';
+const SUPABASE_KEY = 'sb_publishable_H8hrxemmpXS-z0zKYKkXnQ_boDo9pmu';
