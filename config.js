24292e40e1a9dc07d7254ec27c70c09933fcc79c
yoondo123 <ekaqo0220@gmail.com>
2026-06-04 18:38:58 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,7 @@
 const SUPABASE_URL = 'https://ewqicunyawgcwdrcutqf.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_H8hrxemmpXS-z0zKYKkXnQ_boDo9pmu';
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
